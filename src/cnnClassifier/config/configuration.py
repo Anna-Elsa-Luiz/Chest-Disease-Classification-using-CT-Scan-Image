5035c7824f208b7da93a9d8d531d10eff3e3e133
Anna Elsa Luiz <annaelsaluiz@gmail.com>
2024-05-02 11:26:28 +0530

    data ingestion completed

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,10 +1,7 @@
 import os
 from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import (DataIngestionConfig,
-                                                PrepareBaseModelConfig,
-                                                TrainingConfig,
-                                                EvaluationConfig)
+from cnnClassifier.entity.config_entity import (DataIngestionConfig)
 
 
 class ConfigurationManager:
