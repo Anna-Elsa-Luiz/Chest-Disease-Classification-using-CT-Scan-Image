4ccf69929f78dd8570ff517a50ea7e267d1bbbfe
Anna Elsa Luiz <annaelsaluiz@gmail.com>
2024-05-03 12:44:38 +0530

    model evaluation completed

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -9,17 +9,15 @@ from cnnClassifier.entity.config_entity import (DataIngestionConfig,
 
 class ConfigurationManager:
     def __init__(
-        self,
-        config_filepath = CONFIG_FILE_PATH,
-        params_filepath = PARAMS_FILE_PATH):
+            self,
+            config_filepath=CONFIG_FILE_PATH,
+            params_filepath=PARAMS_FILE_PATH):
 
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
 
         create_directories([self.config.artifacts_root])
 
-
-    
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
 
@@ -29,15 +27,14 @@ class ConfigurationManager:
             root_dir=config.root_dir,
             source_URL=config.source_URL,
             local_data_file=config.local_data_file,
-            unzip_dir=config.unzip_dir 
+            unzip_dir=config.unzip_dir
         )
 
         return data_ingestion_config
-    
 
     def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
         config = self.config.prepare_base_model
-        
+
         create_directories([config.root_dir])
 
         prepare_base_model_config = PrepareBaseModelConfig(
@@ -52,13 +49,13 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
-    
 
     def get_training_config(self) -> TrainingConfig:
         training = self.config.training
         prepare_base_model = self.config.prepare_base_model
         params = self.params
-        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Chest-CT-Scan-data")
+        training_data = os.path.join(
+            self.config.data_ingestion.unzip_dir, "Chest-CT-Scan-data")
         create_directories([
             Path(training.root_dir)
         ])
@@ -66,7 +63,8 @@ class ConfigurationManager:
         training_config = TrainingConfig(
             root_dir=Path(training.root_dir),
             trained_model_path=Path(training.trained_model_path),
-            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            updated_base_model_path=Path(
+                prepare_base_model.updated_base_model_path),
             training_data=Path(training_data),
             params_epochs=params.EPOCHS,
             params_batch_size=params.BATCH_SIZE,
@@ -75,12 +73,12 @@ class ConfigurationManager:
         )
 
         return training_config
-    
+
     def get_evaluation_config(self) -> EvaluationConfig:
         eval_config = EvaluationConfig(
             path_of_model="artifacts/training/model.h5",
             training_data="artifacts/data_ingestion/Chest-CT-Scan-data",
-            mlflow_uri="https://dagshub.com/entbappy/Chest-Disease-Classification-from-Chest-CT-Scan-Image.mlflow",
+            mlflow_uri="https://dagshub.com/Anna-Elsa-Luiz/Chest-Disease-Classification-using-CT-Scan-Image.mlflow",
             all_params=self.params,
             params_image_size=self.params.IMAGE_SIZE,
             params_batch_size=self.params.BATCH_SIZE
